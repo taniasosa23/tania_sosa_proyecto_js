b84a93b82a3cb33b3040473b42092acaf279e27d
taniasosa23 <taniasosa23@gmail.com>
2023-09-26 20:08:09 -0600

    Segunda-PreentregaCoder

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
-function procesarGastos() {
+
+function Gasto(concepto, cantidad) {
+    this.concepto = concepto;
+    this.cantidad = cantidad;
+}
+
+let historialGastos = [];
+
+function agregarGasto(concepto, cantidad) {
+    let nuevoGasto = new Gasto(concepto, cantidad);
+    historialGastos.push(nuevoGasto);
+}
+
+function calcularTotalGastos() {
     let totalGastos = 0;
+    for (let i = 0; i < historialGastos.length; i++) {
+        totalGastos += historialGastos[i].cantidad;
+    }
+    return totalGastos;
+}
 
-    while(true) {
-        let gasto = prompt("Ingresa un gasto:");
+function simularControlDeGastos() {
+    while (true) {
+        let concepto = prompt("Ingresa el nombre del gasto (o escribe 'salir' para terminar):");
 
-        if (gasto === null) {
-            
+        if (concepto === null || concepto.toLowerCase() === "salir") {
             break;
         }
 
-        if (gasto.toLowerCase() === "salir") {
-            break;
-        }
-
-        let cantidad = parseFloat(prompt("Ingresa la cantidad del gasto: escribe cancelar para terminar"));
+        let cantidad = parseFloat(prompt(`Ingresa la cantidad que se gasto en "${concepto}":`));
 
         if (!isNaN(cantidad)) {
-            totalGastos += cantidad;
+            agregarGasto(concepto, cantidad);
         } else {
             alert("Cantidad inválida. Ingresa un número.");
         }
     }
 
+    let totalGastos = calcularTotalGastos();
     alert(`El total de gastos es: $${totalGastos.toFixed(2)}`);
 }
 
-procesarGastos();
+
+simularControlDeGastos();
