1db3d599191638726f4672aaf95cb8e394c4a4e4
taniasosa23 <taniasosa23@gmail.com>
2023-10-04 21:48:41 -0600

    tercera pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
-
-function Gasto(concepto, cantidad) {
-    this.concepto = concepto;
-    this.cantidad = cantidad;
+class Gasto {
+    constructor(concepto, cantidad, usuario) {
+        this.fecha = new Date();
+        this.concepto = concepto;
+        this.cantidad = cantidad;
+        this.usuario = usuario;
+    }
 }
 
-let historialGastos = [];
+const historialGastos = JSON.parse(localStorage.getItem("historialGastos")) || [];
 
-function agregarGasto(concepto, cantidad) {
-    let nuevoGasto = new Gasto(concepto, cantidad);
+const agregarGasto = (concepto, cantidad, usuario, fecha) => {
+    const nuevoGasto = new Gasto(concepto, cantidad, usuario, fecha);
     historialGastos.push(nuevoGasto);
+    localStorage.setItem("historialGastos", JSON.stringify(historialGastos));
+
+    const tablaDatosCargados = document.getElementById("tablaDatosCargados");
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+        <td>${concepto}</td>
+        <td>${cantidad}</td>
+        <td>${usuario}</td>
+        <td>${fecha}</td>
+    `;
+    tablaDatosCargados.appendChild(fila);
 }
 
-function calcularTotalGastos() {
-    let totalGastos = 0;
-    for (let i = 0; i < historialGastos.length; i++) {
-        totalGastos += historialGastos[i].cantidad;
+const calcularTotalGastos = () => {
+    return historialGastos.reduce((acc, gasto) => acc + gasto.cantidad, 0)
+}
+
+const formCargarGasto = document.getElementById("formCargarGasto");
+formCargarGasto.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const concepto = e.target.elements["concepto"].value;
+    const cantidad = parseFloat(e.target.elements["cantidad"].value);
+    const usuario = e.target.elements["usuario"].value;
+
+    if (!isNaN(cantidad)) {
+        agregarGasto(concepto, cantidad, usuario);
+        formCargarGasto.reset();
+    } else {
+        alert("Cantidad inválida. Ingresa un número.");
     }
-    return totalGastos;
+});
+
+const btnTotalGastos = document.getElementById("btnTotalGastos");
+btnTotalGastos.addEventListener("click", () => {
+    const mostrarTotalGastos = document.getElementById("mostrarTotalGastos");
+    mostrarTotalGastos.innerText = calcularTotalGastos();
+});
+
+const btnBuscarConcepto = document.getElementById("btnBuscarConcepto");
+btnBuscarConcepto.addEventListener("click", () => {
+    const conceptoABuscar = document.getElementById("inputBuscarConcepto").value;
+    const resultados = buscarGastosPorConcepto(conceptoABuscar);
+
+    const tablaResultados = document.getElementById("tablaResultados");
+    tablaResultados.innerHTML = "";
+    resultados.forEach(gasto => {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${gasto.fecha}</td>
+            <td>${gasto.concepto}</td>
+            <td>${gasto.cantidad}</td>
+            <td>${gasto.usuario}</td>
+        `;
+        tablaResultados.appendChild(fila);
+    });
+});
+
+function buscarGastosPorConcepto(concepto) {
+    return historialGastos.filter(gasto => gasto.concepto.toLowerCase().includes(concepto.toLowerCase()));
 }
-
-function simularControlDeGastos() {
-    while (true) {
-        let concepto = prompt("Ingresa el nombre del gasto (o escribe 'salir' para terminar):");
-
-        if (concepto === null || concepto.toLowerCase() === "salir") {
-            break;
-        }
-
-        let cantidad = parseFloat(prompt(`Ingresa la cantidad que se gasto en "${concepto}":`));
-
-        if (!isNaN(cantidad)) {
-            agregarGasto(concepto, cantidad);
-        } else {
-            alert("Cantidad inválida. Ingresa un número.");
-        }
-    }
-
-    let totalGastos = calcularTotalGastos();
-    alert(`El total de gastos es: $${totalGastos.toFixed(2)}`);
-}
-
-
-simularControlDeGastos();
